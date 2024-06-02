f7e479f9b721c3c8b8d6a9ee6a10010b2a1a526c
Iurii <yurasik96@inbox.ru>
2024-06-02 20:54:46 +0500

    Stub and tests for add

--- src/main/java/org/example/PhoneBook.java
+++ src/main/java/org/example/PhoneBook.java
@@ -1,5 +1,11 @@
 package org.example;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class PhoneBook {
-    
+    public static Map<String, Long> names_phones = new HashMap<>();
+    public static int add(Map<String, Long> names_phones){
+        return 0;
+    }
 }
--- src/test/java/org/example/PhoneBookTest.java
+++ src/test/java/org/example/PhoneBookTest.java
@@ -1,6 +1,24 @@
 package org.example;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
 class PhoneBookTest {
 
 
+
+    @Test
+    void add_Test() {
+        Map<String, Long> names_phones_Test = new HashMap<>();
+        String name = "Biba";
+        Long phone = 8_800_555_35_35L;
+        //
+        int expected=1;
+        int actual = PhoneBook.add(names_phones_Test);
+        //
+        Assertions.assertEquals(expected,actual);
+    }
 }
